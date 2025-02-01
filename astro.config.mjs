c518ee25c9dd548f1e5e91b6b1d6bd59fb99036b
Riyanbayu123 <riyanbayu0102@gmail.com>
2025-02-01 17:52:20 +0700

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://riyanss.netlify.app",
+  integrations: [mdx(), sitemap(), tailwind()],
+  adapter: netlify(),
+});
---
+++ src/config.ts
@@ -0,0 +1,8 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Riyan Bayu Saputra | Portfolio Website";
+export const SITE_DESCRIPTION =
+  "Software & AI Engineer | Informatics Engineering Students";
+export const GENERATE_SLUG_FROM_TITLE = true;
+export const TRANSITION_API = true;
